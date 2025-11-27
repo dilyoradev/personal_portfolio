f7c5d2b10dd9bbc9bd22bfe9cb6e3c47791fc661
Dilyorakhon <128347223+dilyoradev@users.noreply.github.com>
2025-11-27 11:56:03 +0900

    Added Cookies

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 import os
 from slugify import slugify
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response, session
 from articles import Article
 
 app = Flask(__name__)
+app.secret_key = "thisisverysecret"
 
 
 articles = Article.all()
+
+@app.route("/first-time")
+def first_time():
+    if 'seen' not in request.cookies:
+        response = make_response("You are new here")
+        response.set_cookie('seen', '1')
+        return response
+
+    seen = int(request.cookies['seen'])       
+    response = make_response(f"I have {seen} you times before")
+    response.set_cookie('seen', str(seen+1))
+    return response  
 
 
 @app.route("/")
